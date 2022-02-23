2460bb71cfbf64d0b679f3523f31a01c14a03c22
ftournade <tournade.florent@gmail.com>
2022-02-23 23:41:50 +0100

    stop assume 1D shape for dense layers

--- Layers/FullyConnectedLayer.h
+++ Layers/FullyConnectedLayer.h
@@ -13,34 +13,45 @@ namespace ToyDNN
 			m_OutputShape = TensorShape( _numNeurons, 1, 1 );
 		}
 
+		FullyConnected( const TensorShape& _outputShape )
+		{
+			m_OutputShape = _outputShape;
+		}
+
 		virtual LayerType GetType() const override { return LayerType::FullyConnected; }
 
 		virtual void Setup( const TensorShape& _previousLayerOutputShape ) override
 		{
-			m_InputShape = TensorShape( _previousLayerOutputShape.Size(), 1, 1 ); //Flatten the input tensor
+			m_InputShape = _previousLayerOutputShape;
 
-			m_Weights.resize( m_InputShape.m_SX * m_OutputShape.m_SX );
+			uint32_t inputSize = m_InputShape.Size();
+			uint32_t outputSize = m_OutputShape.Size();
+
+			m_Weights.resize( inputSize * outputSize );
 			m_WeightGradients.resize( m_Weights.size() );
 
-			m_Biases.resize( m_OutputShape.m_SX );
-			m_BiasGradients.resize( m_OutputShape.m_SX );
+			m_Biases.resize( outputSize );
+			m_BiasGradients.resize( outputSize );
 
 			//WeightInit::Xavier( m_InputShape.Size(), m_OutputShape.Size(), m_Weights );
-			WeightInit::He( m_InputShape.Size(), m_OutputShape.Size(), m_Weights );
+			WeightInit::He( inputSize, outputSize, m_Weights );
 
 			std::fill( m_Biases.begin(), m_Biases.end(), 0.0f );
 		}
 
 		virtual void Forward( const Tensor& _in, Tensor& _out ) const override
 		{
+			uint32_t inputSize = m_InputShape.Size();
+			uint32_t outputSize = m_OutputShape.Size();
+
 			//#pragma omp parallel for
-			for( int i = 0 ; i < (int)m_OutputShape.m_SX ; ++i )
+			for( int i = 0 ; i < (int)outputSize ; ++i )
 			{
 				Scalar netSum = m_Biases[i];
 
-				uint32_t weightIdx = i * m_InputShape.m_SX;
+				uint32_t weightIdx = i * inputSize;
 
-				for( uint32_t j = 0 ; j < m_InputShape.m_SX ; ++j )
+				for( uint32_t j = 0 ; j < inputSize ; ++j )
 				{
 					netSum += _in[j] * m_Weights[weightIdx + j];
 				}
@@ -51,24 +62,24 @@ namespace ToyDNN
 
 		virtual void BackPropagation( const Tensor& _layerInputs, const Tensor& _outputGradients, Tensor& _inputGradients ) override
 		{
-			assert( m_InputShape.m_SY == 1 ); //TODO
-			assert( m_InputShape.m_SZ == 1 ); //TODO
+			uint32_t inputSize = m_InputShape.Size();
+			uint32_t outputSize = m_OutputShape.Size();
 
 			std::fill( _inputGradients.begin(), _inputGradients.end(), Scalar( 0.0 ) );
 
 			//#pragma omp parallel for
-			for( int i = 0 ; i < (int)m_OutputShape.m_SX ; ++i )
+			for( int i = 0 ; i < (int)outputSize ; ++i )
 			{
 				Scalar dE_dN = _outputGradients[i];
 
 				m_BiasGradients[i] += dE_dN; /*dN_dB is ignored because it is 1*/
 
-				for( uint32_t j = 0 ; j < m_InputShape.m_SX ; ++j )
+				for( uint32_t j = 0 ; j < inputSize ; ++j )
 				{
 					Scalar dN_dW = _layerInputs[j];
-					m_WeightGradients[j + i * m_InputShape.m_SX] += dE_dN * dN_dW;
+					m_WeightGradients[j + i * inputSize] += dE_dN * dN_dW;
 
-					_inputGradients[j] += dE_dN * m_Weights[j + i * m_InputShape.m_SX];
+					_inputGradients[j] += dE_dN * m_Weights[j + i * inputSize];
 				}
 
 			}
